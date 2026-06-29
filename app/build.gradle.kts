acb8e45cf479191d9313304edabacd1763fdf428
rd-ex <liuyngchng@hotmail.com>
2026-06-29 21:18:50 +0800

    debug

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,11 +7,7 @@ plugins {
 
 android {
     namespace = "com.rd.rd_app"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 36
 
     // 添加签名配置
     signingConfigs {
